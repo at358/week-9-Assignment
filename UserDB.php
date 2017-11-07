32c2bcc5c1239de9d494411ac8d4b2a8c7e2c949
at358 <at358@njit.edu>
2017-11-06 20:25:38 -0500

    Update UserDB.php

--- UserDB.php
+++ UserDB.php
@@ -14,9 +14,9 @@
 	    $q->execute();
 	    $users = $q->fetchAll();
 	    if($q->rowCount() = 0){
-	      INSERT INTO `accounts`(`id`, `email`, `fname`, `lname`, `phone`, `birthday`, `gender`, `password`) VALUES ([1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8])
+	      INSERT INTO `accounts`(`id`, `email`, `fname`, `lname`, `phone`, `birthday`, `gender`, `password`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7],[value-8])
       	      UPDATE `accounts` SET `id`=[value-1],`email`=[value-2],`fname`=[value-3],`lname`=[value-4],`phone`=[value-5],`birthday`=[value-6],`gender`=[value-7],`password`=[value-8] WHERE 1
-              DELETE FROM `accounts` WHERE 1 
+              DELETE FROM `accounts` WHERE [value-1] 
     		}
 	    }
 	     else{
